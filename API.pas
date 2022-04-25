5143f5ba70fbc1219e084a34cc1a8d4b626a401c
Lelelo1 <42244983+Lelelo1@users.noreply.github.com>
2022-04-25 18:28:49 +0200

    remove weirdly added extra projects

---
+++ API.pas
@@ -0,0 +1,17 @@
+﻿namespace OSMLibrary;
+
+interface
+
+type
+  API = public class
+    var baseUrl: String = "http://overpass-api.de/api/interpreter?data=[out:json];(node(around:3000,57.7027141, 11.916687)[~%22^name%22~%22café%22];);out;";
+  public
+    method search(): String;
+  end;
+
+implementation
+  method API.search: String;
+  begin
+     Http.GetJson(baseUrl);
+  end;
+end.
---
+++ OSMLibrary.pas
@@ -0,0 +1,14 @@
+﻿namespace OSMLibrary;
+
+interface
+
+type
+  OSMLibrary = public class
+    var baseUrl: String = "http://overpass-api.de/api/interpreter?data=[out:json];(node(around:3000,57.7027141, 11.916687)[~"^name"~"café"];);out;";
+  public
+    method search(): String;
+  end;
+
+implementation
+  method Class1.
+end.
